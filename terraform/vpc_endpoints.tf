727491a697a20dfbeb6fb93f5ebc028966446599
Simon Faillace Mullen <22826651+sfmullen@users.noreply.github.com>
2026-08-03 14:37:06 -0300

    fix(terraform): remove the retired AMP (aps-workspaces) VPC endpoints (#2083)

--- terraform/vpc_endpoints.tf
+++ terraform/vpc_endpoints.tf
@@ -1,5 +1,9 @@
 # Security group IDs are literal for now - those security groups aren't
 # imported yet (next layer).
+#
+# The aps-workspaces (AMP) interface endpoints originally imported here have
+# since been deleted along with their security groups - AMP itself was
+# retired, so nothing used them.
 
 resource "aws_vpc_endpoint" "main_s3" {
   vpc_id            = aws_vpc.main.id
@@ -55,21 +59,6 @@ resource "aws_vpc_endpoint" "main_ecr_dkr" {
   }
 }
 
-resource "aws_vpc_endpoint" "main_aps_workspaces" {
-  vpc_id              = aws_vpc.main.id
-  service_name        = "com.amazonaws.us-east-1.aps-workspaces"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-
-  subnet_ids = [
-    aws_subnet.main_private4_1a.id,
-    aws_subnet.main_private5_1b.id,
-    aws_subnet.main_private6_1c.id,
-  ]
-
-  security_group_ids = ["sg-048624221709749ef"]
-}
-
 resource "aws_vpc_endpoint" "previewkit_s3" {
   vpc_id            = aws_vpc.previewkit.id
   service_name      = "com.amazonaws.us-east-1.s3"
@@ -123,17 +112,3 @@ resource "aws_vpc_endpoint" "previewkit_ecr_dkr" {
   }
 }
 
-resource "aws_vpc_endpoint" "previewkit_aps_workspaces" {
-  vpc_id              = aws_vpc.previewkit.id
-  service_name        = "com.amazonaws.us-east-1.aps-workspaces"
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-
-  subnet_ids = [
-    aws_subnet.previewkit_private1_1a.id,
-    aws_subnet.previewkit_private2_1b.id,
-    aws_subnet.previewkit_private3_1c.id,
-  ]
-
-  security_group_ids = ["sg-0e74f310e87ab0f90"]
-}
