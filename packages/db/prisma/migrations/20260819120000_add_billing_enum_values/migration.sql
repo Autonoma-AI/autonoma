7e65c7864268c59f9844d49a476ad0802608927f
axel escalada <87334103+axlEscalada@users.noreply.github.com>
2026-08-19 15:24:52 -0300

    feat: add transaction type enum values (#2668)

---
+++ packages/db/prisma/migrations/20260819120000_add_billing_enum_values/migration.sql
@@ -0,0 +1,6 @@
+-- AlterEnum
+ALTER TYPE "credit_transaction_type" ADD VALUE 'AI_COST_CONSUMPTION';
+ALTER TYPE "credit_transaction_type" ADD VALUE 'PREVIEW_BUILD_CONSUMPTION';
+
+-- AlterEnum
+ALTER TYPE "github_pr_comment_kind" ADD VALUE 'credits';
